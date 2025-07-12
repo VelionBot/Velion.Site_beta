ee8fe518709751bdb7c93a255d39eb06f590dce6
AKAEMIS <80256449+bladn3ss@users.noreply.github.com>
2025-07-12 21:04:23 +0500

    321

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def logout():
     return redirect(url_for('index'))
 
 def run_flask():
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
