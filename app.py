3a6b53b136ee29a4a4247d2aea922830f902fc24
AKAEMIS <80256449+bladn3ss@users.noreply.github.com>
2025-07-12 21:16:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -123,8 +123,8 @@ def logout():
     return redirect(url_for('index'))
 
 def run_flask():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='100.20.92.101', port=port)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     # Start Discord bot in background
